3f69a901f6c39c24f9ef6f3b907ff8b0e9454d86
edocico <138378767+edocico@users.noreply.github.com>
2023-11-06 10:44:03 +0100

    add message title

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      message: "La mia prima app con Vite",
+    };
+  },
+};
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>{{ message }}</h1>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
